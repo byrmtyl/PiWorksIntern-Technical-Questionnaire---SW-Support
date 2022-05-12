f684af1c9817c7876480a05b5d4bab4a89ef8440
bayra <bayra@DESKTOP-9ESC7CU>
2022-05-12 03:44:48 +0300

    Proje dosyası ekle.

---
+++ PiWorks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class is_divisible_Pi
+{
+    public static void Main()
+    {
+        Console.Write("Example Input : ");
+        string input = Console.ReadLine();
+        int number = Convert.ToInt32(input);
+        int rem = 0, sum = 0, x;
+        double output;
+
+        x = number;
+
+        while (number > 0)
+        {
+            rem = number % 10;
+            sum = sum + rem;
+            number = number / 10;
+        }
+        output = x / sum;
+        if (x % sum == 0)
+            Console.WriteLine("{0} is disible by {1} (result: {2}) |True",x,sum, output);
+        else
+            Console.WriteLine("{0} is not a disible by {1} (result: {2}) |False", x, sum, output);
+    }
+}
